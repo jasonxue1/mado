d3903e58d8626e49fcf343352f4b26ed8e94ebe5
Akiomi Kamakura <akiomik@gmail.com>
2024-12-07 22:26:18 +0900

    Update Rule#check for MD001

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use pulldown_cmark::{html::push_html, Parser};
+use markdown::mdast::Node;
 
 use markdownlint::Rule;
 
 fn main() {
-    let parser = Parser::new("hello world");
+    let text = "# Hello
+### Cluel
+## World
+";
+    let doc: Node = markdown::to_mdast(text, &markdown::ParseOptions::default()).unwrap();
 
-    let mut html_output = String::new();
-    push_html(&mut html_output, parser);
-
-    Rule {
+    let md001 = Rule {
         name: "MD001".to_string(),
         description: "Header levels should only increment by one level at a time".to_string(),
         tags: vec!["headers".to_string()],
         aliases: vec!["header-increment".to_string()],
-        check: { |_doc| vec![0] },
+        check: {
+            |root| match root.children() {
+                Some(children) => {
+                    children
+                        .iter()
+                        .fold((vec![], 0), |(acc, old_level), node| match node {
+                            Node::Heading(heading) => {
+                                let mut vec = acc.clone();
+                                if heading.depth < old_level {
+                                    // TODO: Don't use unwrap
+                                    vec.push(heading.position.clone().unwrap());
+                                }
+                                (vec, heading.depth)
+                            }
+                            _ => (acc, old_level),
+                        })
+                        .0
+                }
+                None => vec![],
+            }
+        },
     };
 
-    println!("{html_output}");
+    println!("{:?}", (md001.check)(&doc));
+    println!("{:?}", doc);
 }
--- src/rule.rs
+++ src/rule.rs
@@ -1,9 +1,7 @@
-use pulldown_cmark::Parser;
-
 pub struct Rule {
     pub name: String,
     pub description: String,
     pub tags: Vec<String>,
     pub aliases: Vec<String>,
-    pub check: fn(doc: Parser) -> Vec<usize>,
+    pub check: fn(doc: &markdown::mdast::Node) -> Vec<markdown::unist::Position>,
 }
