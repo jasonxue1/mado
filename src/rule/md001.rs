055ffc21d6513b80b8c532525f47f267f4375ad9
Akiomi Kamakura <akiomik@gmail.com>
2024-12-08 15:49:44 +0900

    Fix MD001

--- src/rule/md001.rs
+++ src/rule/md001.rs
@@ -39,7 +39,7 @@ impl Rule for MD001 {
                     .fold((vec![], 0), |(acc, old_depth), node| match node {
                         Node::Heading(heading) => {
                             let mut vec = acc.clone();
-                            if heading.depth != old_depth + 1 {
+                            if heading.depth > old_depth + 1 {
                                 let violation = Violation::new(
                                     self.name(),
                                     self.description(),
@@ -59,5 +59,50 @@ impl Rule for MD001 {
             }
             None => Ok(vec![]),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use markdown::{unist::Position, ParseOptions};
+
+    use super::*;
+
+    #[test]
+    fn check_errors() {
+        let text = "# Header 1
+
+### Header 3
+
+We skipped out a 2nd level header in this document";
+        let doc = markdown::to_mdast(text, &ParseOptions::default()).unwrap();
+        let rule = MD001::new();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![Violation::new(
+            "MD001".to_string(),
+            "Header levels should only increment by one level at a time".to_string(),
+            Position::new(3, 1, 12, 3, 13, 24),
+        )];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_no_errors() {
+        let text = "# Header 1
+
+## Header 2
+
+### Header 3
+
+#### Header 4
+
+## Another Header 2
+
+### Another Header 3";
+        let doc = markdown::to_mdast(text, &ParseOptions::default()).unwrap();
+        let rule = MD001::new();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![];
+        assert_eq!(actual, expected);
     }
 }
