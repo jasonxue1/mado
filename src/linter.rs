f36d5a7d33e8372480b3449280cacce2f8fbce29
Akiomi Kamakura <akiomik@gmail.com>
2024-12-08 13:31:54 +0900

    Improve Linter

--- src/linter.rs
+++ src/linter.rs
@@ -1,6 +1,7 @@
-use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
+
+use markdown::ParseOptions;
 
 use crate::rule;
 use crate::violation::Violation;
@@ -19,23 +20,16 @@ impl Linter {
     }
 
     pub fn check(&self, path: &Path) -> Vec<Violation> {
-        if path.is_file() && path.extension() == Some(OsStr::new("md")) {
-            // TODO: Don't use unwrap
-            let text = &fs::read_to_string(path).unwrap();
-            let doc = markdown::to_mdast(text, &markdown::ParseOptions::default()).unwrap();
-            self.rules
-                .iter()
-                .flat_map(|rule| rule.check(&doc))
-                .collect()
-        } else if path.is_dir() {
-            // TODO: Don't use unwrap
-            let files = fs::read_dir(path).unwrap();
-            files
-                .flat_map(|file| self.check(&file.unwrap().path()))
-                .collect()
-        } else {
-            // TODO
-            vec![]
+        if !path.is_file() || path.extension() != Some("md".as_ref()) {
+            panic!("Unexpected file: {:?}", path);
         }
+
+        // TODO: Don't use unwrap
+        let text = &fs::read_to_string(path).unwrap();
+        let doc = markdown::to_mdast(text, &ParseOptions::default()).unwrap();
+        self.rules
+            .iter()
+            .flat_map(|rule| rule.check(&doc))
+            .collect()
     }
 }
