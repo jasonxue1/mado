5fadf544f14a4d4c541817e2427085115a188404
Akiomi Kamakura <akiomik@gmail.com>
2024-12-18 02:49:41 +0900

    Use mimalloc and tikv-jemallocator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-#![warn(clippy::pedantic)]
+#[cfg(all(
+    not(target_os = "windows"),
+    not(target_os = "openbsd"),
+    not(target_os = "aix"),
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64"
+    )
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+#[cfg(target_os = "windows")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use std::process::ExitCode;
 
