a093b2fb1340bc851401edbf1ed2331c1c3be028
Akiomi Kamakura <akiomik@gmail.com>
2024-12-20 03:43:39 +0900

    Fix MD046

--- src/rule/md046.rs
+++ src/rule/md046.rs
@@ -70,7 +70,7 @@ impl RuleLike for MD046 {
                     CodeBlockStyle::Fenced => !code.fenced,
                     CodeBlockStyle::Indented => code.fenced,
                     CodeBlockStyle::Consistent => match maybe_first_fenced {
-                        Some(fenced) => code.fenced == fenced,
+                        Some(fenced) => code.fenced != fenced,
                         None => false,
                     },
                 };
@@ -101,7 +101,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_errors() {
+    fn check_errors_with_fenced() {
         let text = "Some text.
 
     Code block
@@ -123,7 +123,58 @@ Some more text."
     }
 
     #[test]
-    fn check_no_errors() {
+    fn check_errors_with_indented() {
+        let text = "Some text.
+
+```ruby
+Code block
+```
+
+Some more text."
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD046::new(CodeBlockStyle::Indented);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![rule.to_violation(path, Sourcepos::from((3, 1, 5, 3)))];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_errors_with_consistent() {
+        let text = "Some text.
+
+```ruby
+Code block
+```
+Some more text.
+
+    Code block
+
+Some more more text."
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD046::new(CodeBlockStyle::Consistent);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![rule.to_violation(path, Sourcepos::from((8, 5, 9, 0)))];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_no_errors_with_fenced() {
         let text = "Some text.
 
 ```ruby
@@ -137,6 +188,71 @@ Some more text."
         let ast = parse_document(&arena, &text, &Options::default());
         let doc = Document { path, ast, text };
         let rule = MD046::default();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_no_errors_with_indented() {
+        let text = "Some text.
+
+    Code block
+
+Some more text."
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document { path, ast, text };
+        let rule = MD046::new(CodeBlockStyle::Indented);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_no_errors_with_consistent_fenced() {
+        let text = "Some text.
+
+```ruby
+Code block
+```
+Some more text.
+
+```ruby
+Code block
+```
+
+Some more more text."
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document { path, ast, text };
+        let rule = MD046::new(CodeBlockStyle::Consistent);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_no_errors_with_consistent_indented() {
+        let text = "Some text.
+
+    Code block
+
+Some more text.
+
+    Code block
+
+Some more more text."
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document { path, ast, text };
+        let rule = MD046::new(CodeBlockStyle::Consistent);
         let actual = rule.check(&doc).unwrap();
         let expected = vec![];
         assert_eq!(actual, expected);
