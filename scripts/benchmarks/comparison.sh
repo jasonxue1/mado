dfd918af920dab133b7a8cb49cf133596e9da4ff
Akiomi Kamakura <akiomik@gmail.com>
2024-12-19 13:21:38 +0900

    Fix scripts/benchmarks/comparison.sh

--- scripts/benchmarks/comparison.sh
+++ scripts/benchmarks/comparison.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PROJECT_ROOT=($SCRIPT_DIR/../../)
 DATA_ROOT=$SCRIPT_DIR/data
 DOC_PATH=$DATA_ROOT/gitlab/doc
 
@@ -9,6 +10,6 @@ if [ -z ./target/release/downlint ]; then
 fi
 
 hyperfine --ignore-failure --warmup 10 \
-  "./target/release/downlint check $DOC_PATH" \
+  "$PROJECT_ROOT/target/release/downlint check $DOC_PATH" \
   "mdl --config $SCRIPT_DIR/.mdlrc $DOC_PATH" \
   "$SCRIPT_DIR/node_modules/.bin/markdownlint --config $SCRIPT_DIR/.markdownlint.jsonc $DOC_PATH"
