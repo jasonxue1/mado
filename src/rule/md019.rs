9c59a0b71e8da09dc96f6f9102a4dd9b1bb92d9c
Akiomi Kamakura <akiomik@gmail.com>
2024-12-20 22:27:49 +0900

    Add MD019

--- src/rule/md019.rs
+++ src/rule/md019.rs
@@ -1,4 +1,4 @@
-use comrak::nodes::NodeValue;
+use comrak::nodes::{NodeHeading, NodeValue};
 use miette::Result;
 
 use crate::violation::Violation;
@@ -44,14 +44,19 @@ impl RuleLike for MD019 {
         let mut violations = vec![];
 
         for node in doc.ast.children() {
-            if let NodeValue::Heading(heading) = &node.data.borrow().value {
+            if let NodeValue::Heading(NodeHeading {
+                setext: false,
+                level,
+                ..
+            }) = &node.data.borrow().value
+            {
                 if let Some(text_node) = node.first_child() {
                     if let NodeValue::Text(_) = text_node.data.borrow().value {
                         let heading_position = node.data.borrow().sourcepos;
                         let text_position = text_node.data.borrow().sourcepos;
                         let expected_text_offset =
-                            heading_position.start.column + (heading.level as usize) + 1;
-                        if !heading.setext && text_position.start.column > expected_text_offset {
+                            heading_position.start.column + (*level as usize) + 1;
+                        if text_position.start.column > expected_text_offset {
                             let violation = self.to_violation(doc.path.clone(), heading_position);
                             violations.push(violation);
                         }
@@ -87,29 +92,12 @@ mod tests {
             ast,
             text,
         };
-        let rule = MD019::default();
+        let rule = MD019::new();
         let actual = rule.check(&doc).unwrap();
         let expected = vec![
             rule.to_violation(path.clone(), Sourcepos::from((1, 1, 1, 11))),
             rule.to_violation(path, Sourcepos::from((3, 1, 3, 12))),
         ];
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn check_no_errors_for_multiple_children_nodes() {
-        let text = "# Header with `code` and text".to_owned();
-        let path = Path::new("test.md").to_path_buf();
-        let arena = Arena::new();
-        let ast = parse_document(&arena, &text, &Options::default());
-        let doc = Document {
-            path: path.clone(),
-            ast,
-            text,
-        };
-        let rule = MD019::default();
-        let actual = rule.check(&doc).unwrap();
-        let expected = vec![];
         assert_eq!(actual, expected);
     }
 
@@ -123,7 +111,39 @@ mod tests {
         let arena = Arena::new();
         let ast = parse_document(&arena, &text, &Options::default());
         let doc = Document { path, ast, text };
-        let rule = MD019::default();
+        let rule = MD019::new();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_no_errors_with_setext() {
+        let text = "  Header 1
+========"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document { path, ast, text };
+        let rule = MD019::new();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_no_errors_for_multiple_children() {
+        let text = "# Header with `code` and text".to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD019::new();
         let actual = rule.check(&doc).unwrap();
         let expected = vec![];
         assert_eq!(actual, expected);
