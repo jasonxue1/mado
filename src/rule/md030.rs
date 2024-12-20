ece87476ff0973092bc260eabe1be267bbc3eac6
Akiomi Kamakura <akiomik@gmail.com>
2024-12-20 15:39:52 +0900

    Fix MD030

--- src/rule/md030.rs
+++ src/rule/md030.rs
@@ -35,17 +35,32 @@ impl MD030 {
         violations: &mut Vec<Violation>,
     ) {
         for node in root.children() {
-            if let NodeValue::List(_) = node.data.borrow().value {
-                let num_items = node.children().count();
+            if let NodeValue::List(list) = node.data.borrow().value {
                 for item_node in node.children() {
                     if let NodeValue::Item(item) = item_node.data.borrow().value {
-                        let is_violated = match item.list_type {
-                            ListType::Bullet if num_items == 1 => item.padding > self.ul_single + 1,
-                            ListType::Bullet => item.padding > self.ul_multi + 1,
-                            ListType::Ordered if num_items == 1 => {
-                                item.padding > self.ol_single + 2
+                        // true if multiple Paragraph
+                        let mut is_multi = item_node.children().count() > 1;
+
+                        // Check for single Paragraph with multiple lines
+                        if !is_multi {
+                            if let Some(child_node) = item_node.first_child() {
+                                if let NodeValue::Paragraph = child_node.data.borrow().value {
+                                    for inline_node in child_node.children() {
+                                        if let NodeValue::SoftBreak =
+                                            inline_node.data.borrow().value
+                                        {
+                                            is_multi = true;
+                                        }
+                                    }
+                                }
                             }
-                            ListType::Ordered => item.padding > self.ol_multi + 2,
+                        }
+
+                        let is_violated = match (is_multi, list.list_type) {
+                            (true, ListType::Bullet) => item.padding > self.ul_multi + 1,
+                            (true, ListType::Ordered) => item.padding > self.ol_multi + 2,
+                            (false, ListType::Bullet) => item.padding > self.ul_single + 1,
+                            (false, ListType::Ordered) => item.padding > self.ol_single + 2,
                         };
 
                         if is_violated {
@@ -115,7 +130,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_errors() {
+    fn check_errors_ul() {
+        let text = "*   Foo
+    Second paragraph
+*   Bar"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD030::default();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![
+            rule.to_violation(path.clone(), Sourcepos::from((1, 1, 2, 20))),
+            rule.to_violation(path, Sourcepos::from((3, 1, 3, 7))),
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_errors_ul_with_newline() {
         let text = "*   Foo
 
     Second paragraph
@@ -140,9 +178,138 @@ mod tests {
     }
 
     #[test]
+    fn check_errors_ul_with_ul_single() {
+        let text = "*   Foo
+    Second paragraph
+*   Bar"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD030::new(3, 1, 1, 1);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![rule.to_violation(path, Sourcepos::from((1, 1, 2, 20)))];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_errors_ul_with_ul_multi() {
+        let text = "*   Foo
+    Second paragraph
+*   Bar"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD030::new(1, 1, 3, 1);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![rule.to_violation(path, Sourcepos::from((3, 1, 3, 7)))];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_errors_ol() {
+        let text = "1.   Foo
+     Second paragraph
+1.   Bar"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD030::default();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![
+            rule.to_violation(path.clone(), Sourcepos::from((1, 1, 2, 21))),
+            rule.to_violation(path, Sourcepos::from((3, 1, 3, 8))),
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_errors_ol_with_newline() {
+        let text = "1.   Foo
+
+     Second paragraph
+
+1.   Bar"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD030::default();
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![
+            rule.to_violation(path.clone(), Sourcepos::from((1, 1, 4, 0))),
+            rule.to_violation(path, Sourcepos::from((5, 1, 5, 8))),
+        ];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_errors_ol_with_ol_single() {
+        let text = "1.   Foo
+     Second paragraph
+1.   Bar"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD030::new(1, 3, 1, 1);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![rule.to_violation(path, Sourcepos::from((1, 1, 2, 21)))];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_errors_ol_with_ol_multi() {
+        let text = "1.   Foo
+     Second paragraph
+1.   Bar"
+            .to_owned();
+        let path = Path::new("test.md").to_path_buf();
+        let arena = Arena::new();
+        let ast = parse_document(&arena, &text, &Options::default());
+        let doc = Document {
+            path: path.clone(),
+            ast,
+            text,
+        };
+        let rule = MD030::new(1, 1, 1, 3);
+        let actual = rule.check(&doc).unwrap();
+        let expected = vec![rule.to_violation(path, Sourcepos::from((3, 1, 3, 8)))];
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
     fn check_errors_nested() {
         let text = "* Parent list
     1.  Foo
+        Second paragraph
     2.  Bar"
             .to_owned();
         let path = Path::new("test.md").to_path_buf();
@@ -156,8 +323,8 @@ mod tests {
         let rule = MD030::default();
         let actual = rule.check(&doc).unwrap();
         let expected = vec![
-            rule.to_violation(path.clone(), Sourcepos::from((2, 5, 2, 11))),
-            rule.to_violation(path, Sourcepos::from((3, 5, 3, 11))),
+            rule.to_violation(path.clone(), Sourcepos::from((2, 5, 3, 24))),
+            rule.to_violation(path, Sourcepos::from((4, 5, 4, 11))),
         ];
         assert_eq!(actual, expected);
     }
@@ -174,7 +341,8 @@ mod tests {
 
 1. Foo
    * Bar
-1. Baz"
+   * Baz
+1. Qux"
             .to_owned();
         let path = Path::new("test.md").to_path_buf();
         let arena = Arena::new();
