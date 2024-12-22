9c4bbc1061db90886ab3737f29b38f93da650f29
Akiomi Kamakura <akiomik@gmail.com>
2024-12-22 12:15:43 +0900

    Add bench for hard tab search

---
+++ benches/hard_tab_search.rs
@@ -0,0 +1,81 @@
+use core::hint::black_box;
+use std::sync::LazyLock;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use rand::distributions::DistString as _;
+use rand::distributions::Standard;
+use regex::Regex;
+
+static RE: LazyLock<Regex> = LazyLock::new(|| {
+    #[allow(clippy::unwrap_used)]
+    Regex::new("\t").unwrap()
+});
+
+fn regex_captures(s: &str) -> Option<(usize, usize)> {
+    let mut locs = RE.capture_locations();
+    RE.captures_read(&mut locs, s);
+    if let Some((start_column, end_column)) = locs.get(0) {
+        return Some((start_column + 1, end_column));
+    }
+
+    None
+}
+
+fn string_find(s: &str) -> Option<(usize, usize)> {
+    if let Some(index) = s.find('\t') {
+        let column = index + 1;
+        return Some((column, column));
+    }
+
+    None
+}
+
+fn string_match_indices(s: &str) -> Option<(usize, usize)> {
+    if let Some((index, _)) = s.match_indices('\t').next() {
+        let column = index + 1;
+        return Some((column, column));
+    }
+
+    None
+}
+
+fn chars_position(s: &str) -> Option<(usize, usize)> {
+    if let Some(index) = s.chars().position(|c| c == '\t') {
+        let column = index + 1;
+        return Some((column, column));
+    }
+
+    None
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hard_tab_search");
+
+    for i in 1..=3 {
+        let string = Standard.sample_string(&mut rand::thread_rng(), (i as usize).pow(i) * 100);
+
+        group.bench_with_input(BenchmarkId::new("regex_captures", i), &string, |b, s| {
+            b.iter(|| regex_captures(black_box(s)));
+        });
+
+        group.bench_with_input(BenchmarkId::new("string_find", i), &string, |b, s| {
+            b.iter(|| string_find(black_box(s)));
+        });
+
+        group.bench_with_input(
+            BenchmarkId::new("string_match_indices", i),
+            &string,
+            |b, s| {
+                b.iter(|| string_match_indices(black_box(s)));
+            },
+        );
+
+        group.bench_with_input(BenchmarkId::new("chars_position", i), &string, |b, s| {
+            b.iter(|| chars_position(black_box(s)));
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
