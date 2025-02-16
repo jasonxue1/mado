71c656ddc8a2db7f4ffeb284efd82e1255dcf63b
Akiomi Kamakura <akiomik@gmail.com>
2025-02-16 09:46:07 +0900

    test: refactor for WalkParallelBuilder

--- src/service/walker.rs
+++ src/service/walker.rs
@@ -45,33 +45,63 @@ mod tests {
     extern crate alloc;
 
     use alloc::sync::Arc;
-    use std::{path::Path, sync::Mutex};
+    use miette::{Context as _, IntoDiagnostic as _};
+    use std::{
+        path::{Path, PathBuf},
+        sync::Mutex,
+    };
 
     use ignore::{DirEntry, WalkState};
     use pretty_assertions::assert_eq;
 
     use super::WalkParallelBuilder;
 
+    struct PathCollector {
+        paths: Arc<Mutex<Vec<PathBuf>>>,
+    }
+
+    impl PathCollector {
+        fn new() -> Self {
+            Self {
+                paths: Arc::new(Mutex::new(vec![])),
+            }
+        }
+
+        fn gen_visitor(&self) -> impl Fn(Result<DirEntry, ignore::Error>) -> WalkState {
+            let paths = Arc::clone(&self.paths);
+
+            move |either_entry: Result<DirEntry, _>| {
+                if let Ok(entry) = either_entry {
+                    if let Ok(mut paths) = paths.lock() {
+                        paths.push(entry.into_path());
+                    }
+                }
+
+                WalkState::Continue
+            }
+        }
+
+        fn paths(self) -> miette::Result<Vec<PathBuf>> {
+            let mutex = Arc::into_inner(self.paths).wrap_err("failed to get inner ownership")?;
+            mutex.into_inner().into_diagnostic()
+        }
+    }
+
     #[test]
-    #[allow(clippy::unwrap_used)]
-    fn build() -> miette::Result<()> {
+    fn build_and_run() -> miette::Result<()> {
         let paths = vec![
             Path::new("action").to_path_buf(),
             Path::new("mado.toml").to_path_buf(),
             Path::new("README.md").to_path_buf(),
         ];
         let builder = WalkParallelBuilder::build(&paths, true, true)?;
-        let shared_paths = Arc::new(Mutex::new(vec![]));
-        let walker = |either_entry: Result<DirEntry, _>| {
-            if let Ok(entry) = either_entry {
-                shared_paths.lock().unwrap().push(entry.into_path());
-            }
+        let collector = PathCollector::new();
 
-            WalkState::Continue
-        };
-        builder.run(|| Box::new(walker));
-        let mut actual = shared_paths.lock().unwrap().clone();
+        builder.run(|| Box::new(collector.gen_visitor()));
+
+        let mut actual = collector.paths()?;
         actual.sort();
+
         let expected = vec![
             Path::new("README.md").to_path_buf(),
             Path::new("action").to_path_buf(),
